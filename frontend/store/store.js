94b557c16fd0873aee9b7dd2d867b74e66bb6bd7
Julian Compagni Portis <jcompagni@gmail.com>
2017-12-01 14:44:06 -0800

    make production ready

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -3,9 +3,16 @@ import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import rootReducer from '../reducers/root';
 
+const middlewares = [thunk];
+if (process.env.NODE_ENV !== 'production') {
+  // must use 'require' (import only allowed at top of file)
+  const { logger } = require('redux-logger');
+  middlewares.push(logger);
+}
+
 export default (preloadedState)=>(
   createStore(rootReducer,
     preloadedState,
-    applyMiddleware(thunk, logger)
+    applyMiddleware(...middlewares)
   )
 );
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,21 @@
 var path = require("path");
+var webpack = require("webpack");
+var plugins = [];
+
+var prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat( prodPlugins );
 
 module.exports = {
   context: __dirname,
@@ -7,6 +24,7 @@ module.exports = {
     path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
     filename: "bundle.js"
   },
+  plugins: plugins,
   module: {
     loaders: [
       {
